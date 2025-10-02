f52ace8e8917940e137e9ccf9b303c7da6fa2834
mirerakepha <kephamirera16@gmail.com>
2025-10-02 04:18:42 +0300

    finished adding the bot

---
+++ bot/static/script.js
@@ -0,0 +1,12 @@
+
+
+
+    (function(d, m){
+        var kommunicateSettings =
+            {"appId":"260997339306bcb66dc4cd95940e193f8","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
